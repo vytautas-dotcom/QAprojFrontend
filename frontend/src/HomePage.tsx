31369474e4c01420e95781049dba1687c83becf2
vytautas-dotcom <tumas.vytautas@gmail.com>
2021-10-26 20:03:59 +0300

    Import QuestionList into HomePage and populate with getUnansweredQuestions()

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { QuestionList } from "./QuestionList";
+import { getUnansweredQuestions } from "./QuestionData";
 
 export const HomePage = () => (
   <div>
@@ -6,5 +8,6 @@ export const HomePage = () => (
       <h2>Unanswered Questions</h2>
       <button>Ask a question</button>
     </div>
+    <QuestionList data={getUnansweredQuestions()} />
   </div>
 );
