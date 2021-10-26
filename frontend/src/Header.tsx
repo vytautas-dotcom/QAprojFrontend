ee5132d5beae0e82762cb9d78a9181cc9e2df970
vytautas-dotcom <tumas.vytautas@gmail.com>
2021-10-26 18:33:12 +0300

    Add all necessary attributes for Header

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,3 +1,11 @@
 import React from "react";
 
-export const Header = () => <div>header</div>;
+export const Header = () => (
+  <div>
+    <a href="./">Q and A</a>
+    <input type="text" placeholder="Search..." />
+    <a href="./signin">
+      <span>Sign In</span>
+    </a>
+  </div>
+);
