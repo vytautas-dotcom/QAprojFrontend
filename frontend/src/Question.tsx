2104dec880ed78c07024b56a10faa52212f4d538
vytautas-dotcom <tumas.vytautas@gmail.com>
2021-10-26 20:22:25 +0300

    Populate Question component with questions content no longer than 50 characters

--- frontend/src/Question.tsx
+++ frontend/src/Question.tsx
@@ -9,6 +9,11 @@ export const Question = ({ data }: Props) => (
   <div>
     <div>{data.title}</div>
     <div>
+      {data.content.length > 50
+        ? `${data.content.substring(0, 50)}...`
+        : data.content}
+    </div>
+    <div>
       {`Asked by ${
         data.userName
       } on ${data.created.toLocaleDateString()} ${data.created.toLocaleTimeString()}`}
