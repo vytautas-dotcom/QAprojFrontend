e813c3965ee505a8f0e69078845794daf8b18991
vytautas-dotcom <tumas.vytautas@gmail.com>
2021-10-27 19:47:42 +0300

    Get question by id and show it in QuestionPage

--- frontend/src/QuestionData.ts
+++ frontend/src/QuestionData.ts
@@ -54,6 +54,14 @@ export const getUnansweredQuestions = async (): Promise<QuestionData[]> => {
   return questions.filter((q) => q.answers.length === 0);
 };
 
+export const getQuestion = async (
+  questionId: number
+): Promise<QuestionData | null> => {
+  await wait(500);
+  const results = questions.filter((q) => q.questionId === questionId);
+  return results.length === 0 ? null : results[0];
+};
+
 const wait = (ms: number): Promise<void> => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
