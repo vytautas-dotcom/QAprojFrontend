21e21fdc1516a70c4107aa629e2038fca511dc20
vytautas-dotcom <tumas.vytautas@gmail.com>
2021-10-27 11:04:11 +0300

    Add wait function which makes getUnansweredQuestions wait 500ms asynchronously

--- frontend/src/QuestionData.ts
+++ frontend/src/QuestionData.ts
@@ -49,6 +49,11 @@ const questions: QuestionData[] = [
   },
 ];
 
-export const getUnansweredQuestions = (): QuestionData[] => {
+export const getUnansweredQuestions = async (): Promise<QuestionData[]> => {
+  await wait(500);
   return questions.filter((q) => q.answers.length === 0);
+};
+
+const wait = (ms: number): Promise<void> => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 };
