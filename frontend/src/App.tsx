1401aab0b0f50bd6e048f8c380ecb441c087c2b9
vytautas-dotcom <tumas.vytautas@gmail.com>
2021-10-27 18:30:09 +0300

    Add Not Found page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AskPage } from "./AskPage";
 import { SearchPage } from "./SearchPage";
 import { SignInPage } from "./SignInPage";
+import { NotFoundPage } from "./NotFoundPage";
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
           <Route path="search" element={<SearchPage />} />
           <Route path="ask" element={<AskPage />} />
           <Route path="signin" element={<SignInPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
     </BrowserRouter>
