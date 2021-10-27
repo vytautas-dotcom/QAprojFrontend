0b25c44a6b54ff91f201ee64332b26de9b99ad7b
vytautas-dotcom <tumas.vytautas@gmail.com>
2021-10-27 17:58:12 +0300

    Add routes for created blank pages in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,18 +5,30 @@ import { fontFamily, fontSize, gray2 } from "./Styles";
 import { Header } from "./Header";
 import { HomePage } from "./HomePage";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AskPage } from "./AskPage";
+import { SearchPage } from "./SearchPage";
+import { SignInPage } from "./SignInPage";
+
 function App() {
   return (
-    <div
-      css={css`
-        font-family: ${fontFamily};
-        font-size: ${fontSize};
-        color: ${gray2};
-      `}
-    >
-      <Header />
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div
+        css={css`
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          color: ${gray2};
+        `}
+      >
+        <Header />
+        <Routes>
+          <Route path="" element={<HomePage />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route path="ask" element={<AskPage />} />
+          <Route path="signin" element={<SignInPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
