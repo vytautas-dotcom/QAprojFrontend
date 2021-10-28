c1cfba3463a8677fe30278f638d1191bd6e460c7
vytautas-dotcom <tumas.vytautas@gmail.com>
2021-10-28 12:42:31 +0300

    Implement form submission in the ask form

--- frontend/src/AskPage.tsx
+++ frontend/src/AskPage.tsx
@@ -7,10 +7,13 @@ import {
   FormButtonContainer,
   PrimaryButton,
   FieldError,
+  SubmissionSuccess,
 } from "./Styles";
 import { useForm } from "react-hook-form";
 import React from "react";
 import { Page } from "./Page";
+
+import { postQuestion } from "./QuestionData";
 
 type FormData = {
   title: string;
@@ -18,44 +21,67 @@ type FormData = {
 };
 
 export const AskPage = () => {
+  const [successfullySubmitted, setSuccessfullySubmitted] =
+    React.useState(false);
   const {
     register,
-    formState: { errors },
+    formState: { errors, isSubmitting },
+    handleSubmit,
   } = useForm<FormData>({ mode: "onBlur" });
+
+  const submitForm = async (data: FormData) => {
+    const result = await postQuestion({
+      title: data.title,
+      content: data.content,
+      userName: "Tom",
+      created: new Date(),
+    });
+    setSuccessfullySubmitted(result ? true : false);
+  };
+
   return (
     <Page title="Ask a question">
-      <Fieldset>
-        <FieldContainer>
-          <FieldLabel htmlFor="title">Title</FieldLabel>
-          <FieldInput
-            id="title"
-            type="text"
-            {...register("title", { required: true, minLength: 10 })}
-          ></FieldInput>
-          {errors.title && errors.title.type === "required" && (
-            <FieldError>You must enter the question title</FieldError>
+      <form onSubmit={handleSubmit(submitForm)}>
+        <Fieldset disabled={isSubmitting || successfullySubmitted}>
+          <FieldContainer>
+            <FieldLabel htmlFor="title">Title</FieldLabel>
+            <FieldInput
+              id="title"
+              type="text"
+              {...register("title", { required: true, minLength: 10 })}
+            ></FieldInput>
+            {errors.title && errors.title.type === "required" && (
+              <FieldError>You must enter the question title</FieldError>
+            )}
+            {errors.title && errors.title.type === "minLength" && (
+              <FieldError>The title must be at least 10 characters</FieldError>
+            )}
+          </FieldContainer>
+          <FieldContainer>
+            <FieldLabel htmlFor="content">Content</FieldLabel>
+            <FieldTextArea
+              id="content"
+              {...register("content", { required: true, minLength: 50 })}
+            />
+            {errors.content && errors.content.type === "required" && (
+              <FieldError>You must enter the question content</FieldError>
+            )}
+            {errors.content && errors.content.type === "minLength" && (
+              <FieldError>
+                The content must be at least 50 characters
+              </FieldError>
+            )}
+          </FieldContainer>
+          <FormButtonContainer>
+            <PrimaryButton type="submit">Submit Your Question</PrimaryButton>
+          </FormButtonContainer>
+          {successfullySubmitted && (
+            <SubmissionSuccess>
+              Your question was successfully submitted
+            </SubmissionSuccess>
           )}
-          {errors.title && errors.title.type === "minLength" && (
-            <FieldError>The title must be at least 10 characters</FieldError>
-          )}
-        </FieldContainer>
-        <FieldContainer>
-          <FieldLabel htmlFor="content">Content</FieldLabel>
-          <FieldTextArea
-            id="content"
-            {...register("content", { required: true, minLength: 50 })}
-          />
-          {errors.content && errors.content.type === "required" && (
-            <FieldError>You must enter the question content</FieldError>
-          )}
-          {errors.content && errors.content.type === "minLength" && (
-            <FieldError>The content must be at least 50 characters</FieldError>
-          )}
-        </FieldContainer>
-        <FormButtonContainer>
-          <PrimaryButton type="submit">Submit Your Question</PrimaryButton>
-        </FormButtonContainer>
-      </Fieldset>
+        </Fieldset>
+      </form>
     </Page>
   );
 };
