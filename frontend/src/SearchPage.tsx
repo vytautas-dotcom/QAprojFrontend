9e861d491099a6282caca17d037bf9369e877426
vytautas-dotcom <tumas.vytautas@gmail.com>
2021-10-28 19:47:56 +0300

    Connect the search page to the store

--- frontend/src/SearchPage.tsx
+++ frontend/src/SearchPage.tsx
@@ -2,21 +2,30 @@
 import { css } from "@emotion/react";
 import { useSearchParams } from "react-router-dom";
 import { QuestionList } from "./QuestionList";
-import { searchQuestions, QuestionData } from "./QuestionData";
+import { searchQuestions } from "./QuestionData";
 import { Page } from "./Page";
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  AppState,
+  searchingQuestionsAction,
+  searchedQuestionsAction,
+} from "./Store";
 
 export const SearchPage = () => {
+  const dispatch = useDispatch();
+  const questions = useSelector((state: AppState) => state.questions.searched);
   const [searchParams] = useSearchParams();
-  const [questions, setQuestions] = React.useState<QuestionData[]>([]);
   const search = searchParams.get("criteria") || "";
 
   React.useEffect(() => {
     const doSearch = async (criteria: string) => {
+      dispatch(searchingQuestionsAction());
       const foundResults = await searchQuestions(criteria);
-      setQuestions(foundResults);
+      dispatch(searchedQuestionsAction(foundResults));
     };
     doSearch(search);
+    //eslint-disable-next-line
   }, [search]);
   return (
     <Page title="Search Results">
